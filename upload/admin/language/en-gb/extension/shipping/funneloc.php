0dba09ab251c220252aaa144d88852013051a278
Yomi <yomi.omotoso@funnel.ng>
2019-01-02 11:48:11 +0100

    Initial commit

---
+++ upload/admin/language/en-gb/extension/shipping/funneloc.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']                            = 'Funnel Shipping Method for Opencart';
+
+// Text
+$_['text_extension']                           = 'Extension';
+$_['text_success']                             = 'Success: You have modified Funnel Shipping Method for Opencart!';
+$_['text_edit']                                = 'Edit Funnel Shipping Method';
+// Entry
+$_['entry_api_key']                            = 'API Key';
+$_['entry_status']                             = 'Status';
+$_['entry_sort_order']                         = 'Sort Order';
+
+// Help
+$_['help_api_key']                             = 'Enter the API key assigned to you by Funnel Shipping for Opencart.';
+
+// Error
+$_['error_permission']                         = 'Warning: You do not have permission to modify Funnel Shipping for Opencart shipping!';
+$_['error_api_key']                            = 'Access API Key Required!';
+$_['error_status']                              = 'Status Required!';
+$_['error_sort_order']                          =  'Sort Order Required!';
